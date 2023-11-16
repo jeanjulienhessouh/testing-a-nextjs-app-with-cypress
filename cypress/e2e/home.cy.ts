a31d630ce17321c523b1114c325e8feab23a9659
Jean Julien <me@jeanjulienhessouh.com>
2023-11-16 23:46:28 +0100

    tests navigation each courses page

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -4,15 +4,34 @@ describe("home page", () => {
     cy.visit("http://localhost:3000")
   })
 
-  it("checks if the h1 contains the expected text", () => {
-    cy.getByData("hero-heading")
-      .should("exist")
-      .contains("Testing Next.js Applications with Cypress")
+  context("Hero Section", () => {
+    it("checks if the h1 contains the expected text", () => {
+      cy.getByData("hero-heading")
+        .should("exist")
+        .contains("Testing Next.js Applications with Cypress")
+    })
+
+    it("the features listed on the homepage are correct", () => {
+      cy.get("dt").eq(0).contains("4 Courses")
+      cy.get("dt").eq(1).contains("25+ Lessons")
+      cy.get("dt").eq(2).contains("Free and Open Source")
+    })
   })
 
-  it("the features listed on the homepage are correct", () => {
-    cy.get("dt").eq(0).contains("4 Courses")
-    cy.get("dt").eq(1).contains("25+ Lessons")
-    cy.get("dt").eq(2).contains("Free and Open Source")
+  context("Courses Section", () => {
+    it("course: Testing Your First Next.js Application", () => {
+      cy.getByData("course-0").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/testing-your-first-application")
+    })
+
+    it("course: Testing Foundations", () => {
+      cy.getByData("course-1").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/testing-foundations")
+    })
+
+    it("course: Cypress Fundamentals", () => {
+      cy.getByData("course-2").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/cypress-fundamentals")
+    })
   })
 })
--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -10,27 +10,25 @@ describe("subscribe to newsletter", () => {
     cy.getByData("submit-button").click()
     cy.getByData("success-message")
       .should("be.visible")
-      // verify that success message includes a specific text
       .should("include.text", "has been successfully subscribed")
-      /* verify that success message includes "Success"
-      too much assertion I should delete one of them */
       .invoke("text")
       .should("match", /^Success/)
   })
 
   it("returns an error when the input field is blank", () => {
     cy.getByData("submit-button").click()
-    // verify the form displays an error message when things go wrong
     cy.getByData("error-message")
       .should("be.visible")
       .contains("Email is required")
   })
 
-  it("returns an error when the user enters a bad email)", () => {
-    // user enters a bad email
+  it("returns an error when the user enters a bad email", () => {
     cy.getByData("email-input").type("this is not an email")
     cy.getByData("submit-button").click()
-    // TODO improve this assertion
     cy.getByData("success-message").should("not.exist")
+  })
+
+  it("should not allow an already subscribed user to subscribe again", () => {
+    // TODO
   })
 })
